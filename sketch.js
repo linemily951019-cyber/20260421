5bf8e48b3d1c5388d5703052340e29b1cf3a1486
linemily951019-cyber <linemily951019@gmail.com>
2026-04-21 10:27:15 +0800

    1.2修改攝影機左右顛倒問題

--- sketch.js
+++ sketch.js
@@ -13,6 +13,10 @@ function draw() {
   let imgHeight = height * 0.75;
   
   // 設定影像繪製模式為以中心為基準，並畫在畫布正中央
+  push();
+  translate(width / 2, height / 2); // 將座標原點移至畫面中央
+  scale(-1, 1); // 進行水平翻轉 (X軸乘上 -1)
   imageMode(CENTER);
-  image(capture, width / 2, height / 2, imgWidth, imgHeight);
+  image(capture, 0, 0, imgWidth, imgHeight); // 因為原點已移至中央，直接在 (0, 0) 畫出影像即可
+  pop(); // 恢復原本的座標系統，避免影響其他繪圖
 }
